cc66114121d13a949f95bb482224827b69639351
JohnMasapantaPozo <john.masapanta@hotmail.com>
2024-01-20 14:05:33 +0100

    Add Create and Update Dtos

--- Repositories/IItemsRepository.cs
+++ Repositories/IItemsRepository.cs
@@ -7,5 +7,11 @@ namespace WebApiDEMO.Repositories
     {
         Item GetItem(Guid id);
         IEnumerable<Item> GetItems();
+
+        void CreateItem(Item item);
+
+        void UpdateItem(Item item);
+
+        void DeleteItem(Guid id);
     }
 }
